da62ffc43d88f30119b29860bdd9d46ef1ef4802
CHABAT ESTELLE <chabat.estelle@gmail.com>
2021-04-13 08:44:13 +0200

    Fetch Simpsons Quetes

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import QuoteCard from './components/quoteCard';
+
+
+const quote = {
+  quote: "I can't even say the word 'titmouse' without gigggling like a schoolgirl.",
+  character: "Homer Simpson",
+  image: "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939",
+  characterDirection: "Right",
+  };
+
+function App() {
+
+  const [simpson, setSimpson] = useState(quote);
+  const getSimpson = () => {
+    // Send the request
+    axios
+      .get('https://simpsons-quotes-api.herokuapp.com/quotes?count=num')
+      // Extract the DATA from the received response
+      .then((response) => response.data)
+      // Use this data to update the state
+      .then((data) => {
+        setSimpson(data.results[0]);
+      });
+  };
+
+
+ 
+  return (
+    <div className='App'>
+    <QuoteCard simpson ={simpson}/>
+    <button type="button" onClick={getSimpson}> Simpson Quote </button>
+     </div>
+  )
+}
+
+export default App
---
+++ src/components/quoteCard.jsx
@@ -0,0 +1,26 @@
+
+import React from 'react';
+
+function QuoteCard({ simpson }) {
+  return (
+    simpson && (
+      <div className='QuoteCard'>
+        <img src={simpson.image} alt={simpson.character} />
+        <ul>
+          <li>Character: {simpson.character}</li>
+          <li>
+            Quote: {simpson.quote}
+          </li>
+          <li>Character Direction: {simpson.characterDirection}</li>
+        
+        </ul>
+      </div>
+    )
+  );
+};
+
+export default QuoteCard;
+
+//The application should include: A button to retrieve a quote,
+// an area where the name of the character, the photo and the quote is displayed 
+//(it is possible to reuse code from previous quests, namely the QuoteCard component)
